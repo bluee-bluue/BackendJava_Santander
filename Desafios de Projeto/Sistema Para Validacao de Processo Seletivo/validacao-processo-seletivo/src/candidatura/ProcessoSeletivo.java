5bae70535bd3b870a6edf101e84997e3ecbb123c
Sergio Caique <scaique080@gmail.com>
2023-09-17 00:23:12 -0300

    Novo Projeto: Criando Um Pequeno Sistema Para Validação de Processo Seletivo

---
+++ Desafios de Projeto/Sistema Para Validacao de Processo Seletivo/validacao-processo-seletivo/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,100 @@
+package candidatura;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        //selecaoCondidatos();
+        //imprimirSelecionados();
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "CAIQUE"};
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuaTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuaTentando = !atendeu;
+            if (continuaTentando) {
+                tentativasRealizadas++;
+            }
+            else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        }
+        while (continuaTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        }
+        else {
+            System.out.println("NÂO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MAXIMO DE TENTATIVAS: " + tentativasRealizadas + " TENTATIVAS REALIZADAS");
+        }
+    }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "CAIQUE"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        /*for (int i=0; i < candidatos.length; i++) {
+            System.out.println("O candidato de número " + (i+1) + " é " + candidatos[i]);
+        }*/
+
+        for (String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+
+    static void selecaoCondidatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "CAIQUE"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            String salarioFormatado = String.format("%.2f", salarioPretendido);
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioFormatado);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga\n");
+                candidatosSelecionados++;
+            }
+            else {
+                System.out.println("O candidato " + candidato + " NÂO foi selecionado para a vaga\n");
+            }
+
+            candidatoAtual++;
+        }
+    }
+    
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1500, 2500);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }
+        else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Santander Bootcamp 2023 - Backend Java
-## Desafios de Projeto (2/6)
+## Desafios de Projeto (3/6)
 1. Contribuindo em um Projeto Open Source no GitHub. [[Repositório]](https://github.com/bluee-bluue/dio-lab-open-source)
 2. Simulando uma Conta Bancária Através do terminal/Console. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Projeto/Simulando%20Uma%20Conta%20Banc%C3%A1ria)
-3. Criando um Pequeno Sistema para Validação de Processo Seletivo. [Desafio de projeto não realizado]
+3. Criando um Pequeno Sistema para Validação de Processo Seletivo. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Projeto/Sistema%20Para%20Validacao%20de%20Processo%20Seletivo/)
 4. Orientação a Objetos e UML: Diagramação de Classes do iPhone. [Desafio de projeto não realizado]
 5. Explorando Padrões de Projetos na Prática com Java. [Desafio de projeto não realizado]
 6. Publicando Sua API REST na Nuvem Usando Spring Boot 3, Java 17 e Railway. [Desafio de projeto não realizado]
