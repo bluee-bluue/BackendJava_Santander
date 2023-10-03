f6b6fa089004b0dfe461a7bd6f100e5d4681abf7
Sergio Caique <scaique080@gmail.com>
2023-10-03 20:25:31 -0300

    Novos Desafios de Código: CoAbrindo Contas, fres Seguros, Herança Bancária, Reunião de Acionistas, A eÚltima Transação

---
+++ Desafios de Codigo/src/CofresSeguros/CofresSeguros.java
@@ -0,0 +1,63 @@
+package CofresSeguros;
+
+import java.util.Scanner;
+
+abstract class Cofre {
+    protected String tipo;
+    protected String metodoAbertura;
+
+    public Cofre(String tipo, String metodoAbertura) {
+        this.tipo = tipo;
+        this.metodoAbertura = metodoAbertura;
+    }
+
+    public void imprimirInformacoes() {
+        System.out.println("Tipo: " + this.tipo);
+        System.out.println("Metodo de abertura: " + this.metodoAbertura);
+    }
+}
+
+class CofreDigital extends Cofre {
+
+    private int senha;
+
+    public CofreDigital(int senha) {
+        super("Cofre Digital", "Senha");
+        this.senha = senha;
+    }
+}
+
+class CofreFisico extends Cofre {
+
+    public CofreFisico() {
+        super("Cofre Fisico", "Chave");
+    }
+
+}
+
+public class CofresSeguros {
+    public static void main(String[] args) {
+        // Lê o tipo de cofre (primeira linha da entrada)
+        Scanner scanner = new Scanner(System.in);
+        String tipoCofre = scanner.nextLine();
+
+        // TODO: Implemente a condição necessário para a verificação dos cofres seguros:
+        if (tipoCofre.equalsIgnoreCase("digital")) {
+            int senha = scanner.nextInt();
+            int senhaConfirma = scanner.nextInt();
+
+            if (senha == senhaConfirma) {
+                CofreDigital cofreDigital = new CofreDigital(senha);
+                cofreDigital.imprimirInformacoes();
+                System.out.println("Cofre aberto!");
+            } else {
+                CofreDigital cofreDigital = new CofreDigital(senha);
+                cofreDigital.imprimirInformacoes();
+                System.out.println("Senha incorreta!");
+            }
+        } else if (tipoCofre.equalsIgnoreCase("fisico")) {
+            CofreFisico cofreFisico = new CofreFisico();
+            cofreFisico.imprimirInformacoes();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -13,8 +13,8 @@
 3. Condicionalmente Rico. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/CondicionalmenteRico/)
 4. Juros Compostos. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/JurosCompostos/)
 5. O Grande Proposito. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/OGrandeProposito/)
-6. Abrindo Contas. [Desafio de projeto não realizado]
-7. Herança Bancária. [Desafio de projeto não realizado]
-8. Cofres Seguros. [Desafio de projeto não realizado]
-9. Reunião de Acionistas. [Desafio de projeto não realizado]
-10. A Última Transação. [Desafio de projeto não realizado]
+6. Abrindo Contas. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/AbrindoContas/)
+7. Herança Bancária. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/HerancaBancaria/)
+8. Cofres Seguros. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/CofresSeguros/)
+9. Reunião de Acionistas. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/ReuniaoDeAcionistas/)
+10. A Última Transação. [[Repositório]](https://github.com/bluee-bluue/BackendJava_Santander/tree/main/Desafios%20de%20Codigo/src/AUltimaTransacao/)
