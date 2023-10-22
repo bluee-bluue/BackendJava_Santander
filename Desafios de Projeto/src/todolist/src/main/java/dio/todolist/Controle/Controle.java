c63430159c5d5d196d3d2390f32aa28f5e6f3f2f
Sergio Caique <scaique080@gmail.com>
2023-10-22 00:55:51 -0300

    Gerenciador de Tarefas

---
+++ Desafios de Projeto/src/todolist/src/main/java/dio/todolist/Controle/Controle.java
@@ -0,0 +1,25 @@
+package dio.todolist.Controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+public class Controle {
+    /*
+     * Métodos de acesso do HTTP:
+     *
+     * GET - Buscar uma informação
+     * POST - Adicionar um dado/informação
+     * PUT - Alterar dados/informações
+     * DELETE - Remover um dado
+     * PATCH - Alterar somente uma parte da informação/dado
+     *
+     */
+
+    @GetMapping("/")
+    public String primeiraMensagem() {
+        return "Hello World!";
+    }
+}
